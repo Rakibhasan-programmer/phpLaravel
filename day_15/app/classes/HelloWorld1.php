2a58eb01096dbd827470be2c50bdea68c55b72bf
unknown <rakibulhasan.seu.cse@gmail.com>
2022-10-25 14:50:00 +0600

    hello world printed with oop

---
+++ day_15/app/classes/HelloWorld1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\classes;
+
+
+class HelloWorld1
+{
+    public $message = "Hello World";
+    public function printInfo(){
+        echo $this->message;
+    }
+}
--- day_15/index.php
+++ day_15/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once './vendor/autoload.php';
+use App\classes\HelloWorld1;
+
+$helloWorld = new HelloWorld1();
+$helloWorld->printInfo();
+
