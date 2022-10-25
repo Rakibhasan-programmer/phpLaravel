a861dcf3d6032d9b92308785f867c984ca836e7a
unknown <rakibulhasan.seu.cse@gmail.com>
2022-10-25 17:30:07 +0600

    operators in php

--- day_15/app/classes/HelloWorld1.php
+++ day_15/app/classes/HelloWorld1.php
@@ -9,6 +9,10 @@ class HelloWorld1
     public $firstName;
     public $lastName;
 
+    public $x = 10;
+    public $y = 20;
+    public $z = 30;
+
     public function __construct(){
         $this->firstName = "Md Rakibul";
         $this->lastName = "Hasan";
@@ -21,5 +25,66 @@ class HelloWorld1
     public function printInfo(){
         echo $this->message;
         echo "<br>";
+        echo $this->x + $this->y; // 30
+        echo "<br>";
+        echo $this->x - $this->y; // -10
+        echo "<br>";
+        echo $this->x * $this->y; // 200
+        echo "<br>";
+        echo $this->x / $this->y; // .5
+        echo "<br>";
+        echo $this->x % $this->y; // 10
+        echo "<br>";
+
+        echo $this->y--; // 20
+        echo "<br>";
+        echo --$this->y; // 18
+        echo "<br>";
+        echo $this->y--; // 18
+        echo "<br>";
+        echo $this->y--; // 17
+        echo "<br>";
+
+        echo -$this->z;
+        echo "<br>";
+        echo $this->x += $this->y; // 30
+        echo "<br>";
+        echo $this->x -= $this->y; // 10
+        echo "<br>";
+
+        echo $this->x > $this->y; // false
+        echo "<br>";
+        echo $this->x >= $this->y; // false
+        echo "<br>";
+        echo $this->x < $this->y; // true
+        echo "<br>";
+        echo $this->x <= $this->y; // true
+        echo "<br>";
+        echo $this->x = $this->y; // assign y value into x
+        echo "<br>";
+        echo $this->x === $this->y; // false
+        echo "<br>";
+
+        echo (($this->x < $this->y) && ($this->y < $this->z));
+
     }
+
+    //    3 major rules in variables
+    //    1. Start with dollar ($) sign
+    //    2. a-z, A-Z, 0-9, -, _
+    //    3. Not started with number
+
+    //    standard rules
+    //    1. Start with small letter.
+    //    2. Meaningful name.
+    //    3. Readable
+
+    //    operators
+    //    Arithmetic Operators(+, -, /, *, %, **, ++, --)
+    //    Assignment Operator(=, +=, -=, *=, /=)
+    //    Conditional Operator(===,==, <, <=, >, >=, !=, !==)
+    //    Logical Operator(&&, ||, !)
+
+
+
 }
