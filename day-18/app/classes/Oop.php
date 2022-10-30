6420526657271354fb316493874542d846b1902b
unknown <rakibulhasan.seu.cse@gmail.com>
2022-10-30 14:02:06 +0600

    day-18

---
+++ day-18/app/classes/Oop.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Oop
+{
+
+    public function printInfo(){
+        header("Location: action.php?page=home");
+    }
+
+}
---
+++ day-18/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\classes\Oop;
+
+$OOP = new Oop();
+$OOP->printInfo();
